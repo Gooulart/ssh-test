b7d384db87969c07165aa4b34d77be222ed6862e
Gooulart <ggoulart448@gmail.com>
2023-04-17 22:02:09 -0300

    AC2 API

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"Cachorro que late, morde ?"
+# NÃO ENTENDI MUITO BEM COMO FUNCIONA O STATUS CODE
+não entendi se tinha que implementar algum tipo de texto de retorono
+para o status code, ou apenas definir ? realmente não entendi
--- api1.py
+++ api1.py
@@ -1,30 +1,15 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/api")
+@app.route("/", methods=["GET"])
 def home():
-    resultado = "Entre as notas da URL"
+    return "Api Teste"
 
-    primeira = request.args.get('primeira')
-    segunda = request.args.get('segunda')
-
-    if primeira and segunda:
-
-        primeira = float(primeira)
-        segunda = float(segunda)
-
-        media = (primeira + segunda) / 2
-        if media >= 7:
-            resultado = "Aprovado"
-        elif media >= 4 and 7:
-            resultado = "Recuperação"
-        else:
-            resultado = "Reprovado"
-
-    return resultado
-
-
+@app.route('/camiseta')
+def text():
+    return "Vem comprar camiseta da Largatixa"
+    
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
